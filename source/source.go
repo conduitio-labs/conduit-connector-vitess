a9820f7fb4e1f949de5a2eeb5646a0a9b62abe71
alyakimenko <urlucidfall@gmail.com>
2022-08-09 18:04:42 +0300

    log ack method

--- source/source.go
+++ source/source.go
@@ -105,6 +105,8 @@ func (s *Source) Read(ctx context.Context) (sdk.Record, error) {
 // Ack does nothing. We don't need acks for the Snapshot or CDC iterators.
 // It just returns nil here in order to pass the acceptance tests properly.
 func (s *Source) Ack(ctx context.Context, position sdk.Position) error {
+	sdk.Logger(ctx).Debug().Str("position", string(position)).Msg("got ack")
+
 	return nil
 }
 
